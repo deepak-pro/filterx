b663f94260101a6f67e7340c544d94b9d3795116
Deepak JOSHI <joshideepak4598@gmail.com>
2020-05-11 16:32:08 +0530

    Copy files to directories of their extention

--- filterx.sh
+++ filterx.sh
@@ -2,3 +2,9 @@ mkdir files
 ls -p | grep -v / |  rev | cut -d "." -f1 | rev > files/list
 sort files/list | uniq  > files/uniq
 rm files/list
+while read ext;
+do
+        mkdir $ext
+        mv *.$ext $ext/
+done < files/uniq
+rm -rf files
