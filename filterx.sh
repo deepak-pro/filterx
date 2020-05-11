b13219768ad8b1c2da0f5875d5a71381615a392f
Deepak JOSHI <joshideepak4598@gmail.com>
2020-05-11 17:07:18 +0530

    Notify progress to the user

--- filterx.sh
+++ filterx.sh
@@ -4,9 +4,12 @@ utemp=`mktemp`
 ls -p | grep -v / |  rev | cut -d "." -f1 | rev > $temp
 sort $temp | uniq  > $utemp
 rm $temp
+echo "Found `cat $utemp | wc -l | awk '{$1=$1};1'` unique extensions"
 while read ext;
 do
+		echo "Moving all $ext files"
         mkdir $ext
         mv *.$ext $ext/
 done < $utemp
 rm $utemp
+echo "Finished execution"
