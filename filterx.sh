ca14100e18157a95e3924e707fbbd696a4fa0d9a
Deepak JOSHI <joshideepak4598@gmail.com>
2020-05-12 16:55:39 +0530

    Update filterx.sh to correct indentation

--- filterx.sh
+++ filterx.sh
@@ -7,9 +7,9 @@ rm $temp
 echo "Found `cat $utemp | wc -l | awk '{$1=$1};1'` unique extensions"
 while read ext;
 do
-		echo "Moving all $ext files"
-        mkdir $ext
-        mv *.$ext $ext/
+	echo "Moving all $ext files"
+    mkdir $ext
+    mv *.$ext $ext/
 done < $utemp
 rm $utemp
 echo "Finished execution"
